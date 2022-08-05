d20ae63de311a4d0498814364569f65844886d11
yunweneric <yunweneric@gmail.com>
2022-08-05 11:18:13 +0100

    android compart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'dart:io';
-
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:share_plus/share_plus.dart';
