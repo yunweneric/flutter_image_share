58e443ce45dc926f4138e64066204540d22e5a2d
yunweneric <yunweneric@gmail.com>
2022-08-05 11:00:01 +0100

    added image picker

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:share_plus/share_plus.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final ImagePicker _picker = ImagePicker();
+
+  void shareImage(imagePath) {
+    Share.shareFiles([imagePath], text: 'Great picture');
+  }
+
+  Future pickFile() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      File file = File(image.path);
+      return file;
+    } else {
+      return null;
+    }
+  }
+
+  dynamic pickedImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              if (pickedImage != null)
+                Image(
+                  image: FileImage(pickedImage),
+                  height: 200,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  var file = await pickFile();
+                  if (file != null) {
+                    setState(() {
+                      pickedImage = file;
+                    });
+                  }
+                },
+                child: const Text("Pick Image"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (pickedImage != null) Share.shareFiles([pickedImage.path], text: 'Great picture');
+                },
+                child: const Text("Share image"),
+              ),
+            ],
+          ),
+        ));
+  }
+}
